ec655fb97500574ecc29af2e4545b801c01b79e1
Royal Choudhary <royalchoudhary74176@gmail.com>
2023-05-02 15:42:14 +0530

    change the transaction design

--- client/src/components/TransactionList.js
+++ client/src/components/TransactionList.js
@@ -1,28 +1,35 @@
-import React,{useContext,useEffect} from 'react'
-import { GlobalContext } from '../context/GlobalState'
-import { Transaction } from './Transaction';
+import React, { useContext, useEffect } from "react";
+import { GlobalContext } from "../context/GlobalState";
+import { Transaction } from "./Transaction";
 export const TransactionList = () => {
-    const {transactions,gettransactions}=useContext(GlobalContext);
-    const email = JSON.parse(localStorage.getItem("email"));
-    console.log(email);
+  const { transactions, gettransactions } = useContext(GlobalContext);
+  const email = JSON.parse(localStorage.getItem("email"));
+  console.log(email);
 
-    useEffect(()=>{
-      gettransactions(email);
-      // eslint-disable-next-line react-hooks/exhaustive-deps 
-    },[])
-    // console.log(transactions);
+  useEffect(() => {
+    gettransactions(email);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  // console.log(transactions);
 
-    return (
-      <>
+  return (
+    <>
       <h3>History</h3>
-      <ul className="list">
-      
-      {/* IN THIS PLACE WE ADD THE TRANSACTION INSIDE THIS TRANSCATION LIST  */}
+      <ul
+        className="list"
+        style={{
+          overflowY: "scroll",
+          maxHeight: "200px",
+          padding: "0 20px",
+          margin: "10px 0",
+        }}
+      >
+        {/* IN THIS PLACE WE ADD THE TRANSACTION INSIDE THIS TRANSCATION LIST  */}
 
-      {transactions.map((transaction)=>
-        <Transaction key={transaction.id} transaction={transaction}/>
-      )}
-      {/* {transactions.map((transaction)=>
+        {transactions.map((transaction) => (
+          <Transaction key={transaction.id} transaction={transaction} />
+        ))}
+        {/* {transactions.map((transaction)=>
       
       {return(
         <li class="minus">
@@ -31,8 +38,7 @@ export const TransactionList = () => {
       )
       }
       )} */}
-     
       </ul>
-      </>
-    )
-}
+    </>
+  );
+};
